6ff3ec7372af24c868999f697d0f167151ab4d9b
guusvanderham <guus.vanderham@gmail.com>
2021-03-21 17:03:24 +0100

    small changes ROC

---
+++ Assignment-ArtificialImmuneSystems/negative-selection/Task2/Task2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 11:30:28 2021
+
+@author: guusv
+"""
+
+import subprocess
+import os
+import numpy as np
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+
+path = os.getcwd()
